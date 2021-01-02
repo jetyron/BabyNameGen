426801b00703782464659093c66135fc9c59ec32
JET <jetyron@gmail.com>
2021-01-02 12:22:04 -0800

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Baby Name Maker
+
+This was a small project I made one afternoon when my wife and I were having a hard time thinking of a name for our second child. This hasn't helped much, but it was amusing enough that I wanted to share.
+
+## General Info
+This *Name Generator* accepts a last name to add to new name suggestions. A first and middle name are then generated and displayed with the last name on screen. The names will be 3-9 letters long, comprised of randomly constructed syllables. While it does follow some guidelines, its mostly random. As such, do not expect great things (i.e. names that sound like names) to come from this. I mostly did it for a laugh. I might come back to this in the future once my skills have advanced.
---
+++ generator.js
@@ -0,0 +1,30 @@
+const vowels = ['a','e','i','o','u','y'];
+const cons = ['b','c','d','f','g','h','j','k','l','m','n','p','q','r','s','t','v','w','x','z'];
+
+
+const newName = x => {
+    let name = [];
+    if (Math.random() > .9) {
+        name.push(vowels[Math.floor(Math.random()* vowels.length)]);
+    }
+    while (name.length < x) {
+        let i = Math.random() * 100;
+        if (i < 45) {
+            name.push(vowels[Math.floor(Math.random()* vowels.length)]);
+            name.push(cons[Math.floor(Math.random()* cons.length)]);
+        } else  if (i > 55) {
+            name.push(cons[Math.floor(Math.random()* cons.length)]);
+            name.push(vowels[Math.floor(Math.random()* vowels.length)]);
+        } else {
+            name.push(cons[Math.floor(Math.random()* cons.length)]);
+        }
+    }
+    let seg = name.splice(0,x);
+    let init = seg.splice(0,1);
+    return init[0].toUpperCase() + seg.join('');
+}
+
+const babyName = () => {
+    let surName = document.getElementById('name').value;
+    document.getElementById("randomName").innerHTML = `${newName(Math.floor((Math.random()*7)+3))} ${newName(Math.floor((Math.random()*7)+3))} ${surName}`;
+}
